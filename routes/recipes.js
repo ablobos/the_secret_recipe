1987a36ea814336ae35cbdede2bd8b83cd194397
Abdiel "A.B." Villalobos <ablobosportfolio@gmail.com>
2023-02-19 11:41:34 -0500

    im suspecting a package problem due to error log and research

--- routes/recipes.js
+++ routes/recipes.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/:id', function(req, res, next) {
   var id = req.params.id;
   
-  if (id == '1') {
+  if (id == 'pizza') {
   res.render('recipes', {
     title: "Crispy Pizza, oh my!",
     img: 'https://www.simplyrecipes.com/thmb/h7H8UgA28OOJNGiP_bqXCYONB7o=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__09__easy-pepperoni-pizza-lead-4-82c60893fcad4ade906a8a9f59b8da9d.jpg', 
